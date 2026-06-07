0fd04bbae8a4c420296ba142eeb26004dc138bac
Shaad20052027 <shaadstudy2027@gmail.com>
2026-06-07 23:44:40 +0530

    configure render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://streamify-backend-8kp5.onrender.com /api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
